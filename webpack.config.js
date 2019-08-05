f812559135f231779fc3bac724ce73ae5727b527
Alan Magelssen <alanmagelssen@gmail.com>
2019-08-05 15:31:19 -0700

    new build config

--- webpack.config.js
+++ webpack.config.js
@@ -1,128 +1,122 @@
 require('dotenv').config();
-const webpack = require('webpack');
-// Dynamic Script and Style Tags
+const {
+  EnvironmentPlugin,
+} = require('webpack');
+
+const devMode = process.env.NODE_ENV !== 'production';
 const HTMLPlugin = require('html-webpack-plugin');
-// Makes a separate CSS bundle
-const ExtractPlugin = require('extract-text-webpack-plugin');
-const { EnvironmentPlugin, DefinePlugin } = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if (process.env.NODE_ENV === 'test') {
-  require('dotenv').config({ path: '.env.test' })
-} else if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config({ path: '.env.development' })
-}
+const { ProvidePlugin } = require('webpack');
 
-module.exports = () => {
+const plugins = [
+  new EnvironmentPlugin({
+    DATABASE_URL: 'https://townhalltestingsms.firebaseio.com',
+    FIREBASE_API_KEY: 'AIzaSyCJncx2G6bUnecl4H2VHSBTDfRRxg7H5Fs',
+    FIREBASE_AUTH_DOMAIN: 'townhalltestingsms.firebaseapp.com',
+    MESSAGING_SENDER_ID: 86976100332,
+    PROJECT_ID: 'townhalltestingsms',
+    STORAGE_BUCKET: 'townhalltestingsms.appspot.com',
+  }),
+  new ProvidePlugin({
+    jQuery: 'jquery',
+    $: 'jquery',
+    jquery: 'jquery',
+  }),
+  new MiniCssExtractPlugin({
+    // Options similar to the same options in webpackOptions.output
+    // both options are optional
+    chunkFilename: '[id].css',
+    filename: '[name].css',
+  }),
 
-  return {
-
-    // Load this and everythning it cares about
-    entry: `${__dirname}/src/main.js`,
-
-    devtool: 'source-map',
-
-    // Stick it into the "path" folder with that file name
-    output: {
-      filename: 'bundle.[hash].js',
-      path: `${__dirname}/build`,
-      publicPath: '/',
+  new HTMLPlugin({
+    template: `${__dirname}/src/index.html`,
+  }),
+  new CopyWebpackPlugin([
+    // {
+    //   from: 'src/CNAME',
+    // },
+    {
+      flatten: true,
+      from: 'src/assets/images',
+      to: 'images',
     },
+  ]),
+];
 
-    module: {
-      rules: [
-        // If it's a .js file not in node_modules, use the babel-loader
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-        },
-        // If it's a .scss file
-        {
-          test: /\.scss$/,
-          loader: ExtractPlugin.extract({
-            use: [
-              {
-                loader: 'css-loader',
-                options: {
-                  sourceMap: true,
-                },
-              },
-              'resolve-url-loader',
-              {
-                loader: 'sass-loader',
-                options: {
-                  includePaths: [`${__dirname}/src/style`],
-                  sourceMap: true,
-                },
-              },
-            ],
-          }),
-        },
-        {
-          test: /\.less$/,
-          use: [
-            "style-loader",
-            { loader: 'css-loader', options: { sourceMap: 1 } },
-            {
-              loader: "less-loader",
-              options: {
-                javascriptEnabled: true
-              }
-            }
-          ]
-        },
-        {
-          test: /\.css$/,
-          use: [
-            'style-loader',
-            'css-loader',
-            'postcss-loader'
-          ]
-        },
-        {
-          test: /\.(woff|woff2|ttf|eot|glyph|\.svg)$/,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                limit: 10000,
-                name: 'font/[name].[ext]',
-              },
-            },
+module.exports = {
+  mode: 'production',
+
+  plugins,
+  resolve: {
+    extensions: [".jsx", ".js", ".json"]
+  },
+
+  // Load this and everything it cares about
+  entry: `${__dirname}/src/main.js`,
+
+  devtool: 'source-map',
+
+  // Stick it into the "path" folder with that file name
+  output: {
+    filename: 'bundle.[hash].js',
+    path: `${__dirname}/build`,
+  },
+  module: {
+    rules: [
+      // If it's a .js file not in node_modules, use the babel-loader
+      {
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        test: /\.js|.jsx?$/,
+        options: {
+          plugins: [
+            ['import', { libraryName: 'antd', style: 'css' }],
           ],
         },
-        {
-          test: /\.(jpg|jpeg|gif|png|tiff|svg)$/,
-          exclude: /\.glyph.svg/,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                limit: 6000,
-                name: 'image/[name].[ext]',
-              },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot|glyph|\.svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              name: 'font/[name].[ext]',
             },
-          ],
-        },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg|gif|png|tiff|svg)$/,
+        exclude: /\.glyph.svg/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 6000,
+              name: 'images/[name].[ext]',
+            },
+          },
+        ],
+      },
 
-      ],
-    },
-    plugins: [
-      new HTMLPlugin({
-        template: `${__dirname}/src/index.html`,
-      }),
-      new ExtractPlugin('bundle.[hash].css'),
-      new EnvironmentPlugin(['NODE_ENV']),
-      new DefinePlugin({
-        __AUTH_URL__: JSON.stringify(process.env.AUTH_URL),
-        __API_URL__: JSON.stringify(process.env.API_URL),
-        __DEBUG__: JSON.stringify(!process.NODE_ENV),
-      }),
     ],
-    devServer: {
-      historyApiFallback: true,
-    },
-  }
+  },
+  devServer: {
+    historyApiFallback: true,
+    host: '0.0.0.0'
+  },
+
 };
