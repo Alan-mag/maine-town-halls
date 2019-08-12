837d1982a1a8e7b0c29b96c415a9e4aecfdcbce7
Alan Magelssen <alanmagelssen@gmail.com>
2019-08-11 21:27:47 -0700

    subheader copy - line

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -17,7 +17,7 @@ const HeaderBanner = () => (
         <img src="images/lighthouse.png" alt="lighthouse"/>
       </Col>
       <Col sm={16} xs={24}>
-        <p>Senator Collins, it’s been over 20 years since you held an open town hall meeting. In the midst of a month-long Congressional Recess this August, what is more important that meeting with and listen to the Mainers you work for in a public, open setting?  <br /><span className="time-senator">Senator, it’s time.</span>
+        <p>Senator Collins, it’s been over 20 years since you held an open town hall meeting. In the midst of a month-long Congressional Recess this August, what is more important that meeting with and listen to the Mainers you work for in a public, open setting? <span className="time-senator">Senator, it’s time.</span>
         </p>
         <p>Please join your fellow Mainers at these town hall meetings and listen to their priorities, their values, and their questions about your work in Washington, D.C.
         </p>
