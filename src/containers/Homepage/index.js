425b9b8af189c26c5bd6f8b69c7270720199b5af
meganrm <meganrm@gmail.com>
2019-09-09 23:08:56 -0700

    loading past events

--- src/containers/Homepage/index.js
+++ src/containers/Homepage/index.js
@@ -8,48 +8,30 @@ import {
 import EventCard from '../../components/EventCard';
 import MainLayout from '../../components/MainLayout';
 import './style.scss';
-import { firebasedb } from '../../utils/firebase';
-import { eventIds, portlandEvent } from '../../constants';
+import {
+  portlandEvent,
+  bangorTownhall,
+} from '../../constants';
 
 class Homepage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      event1: {},
-      event2: {},
-      loading: true,
-    };
-  }
-  componentDidMount() {
-    const event1Promise = firebasedb.ref(`townHalls/${eventIds[0]}`).once('value');
-    const event2Promise = firebasedb.ref(`townHalls/${eventIds[1]}`).once('value');
-    Promise.all([event1Promise, event2Promise])
-      .then(eventData => eventData.reduce((acc, cur, index) => {
-        acc[`event${index + 1}`] = cur.val();
-        return acc;
-      }, {}))
-      .then((data) => {
-        this.setState({
-          ...data,
-          loading: false,
-        });
-      });
-  }
 
   render() {
-    const { event2, loading } = this.state;
     const event1 = portlandEvent;
+    const event2 = bangorTownhall;
     return (
       <MainLayout>
         <Row gutter={16}>
           <Col xs={24} sm={24} md={24} lg={12} xl={12}>
             <Card title={<h3>Thank you for joining us at our Portland town hall!</h3>} className="event-success-card" />
             <div className="disable-screen">
-              <EventCard eventData={event1} loading={loading} disabled />
+              <EventCard eventData={event1} loading={false} disabled />
             </div>
           </Col>
           <Col xs={24} sm={24} md={24} lg={12} xl={12}>
-            <EventCard eventData={event2} loading={loading} />
+            <Card title={<h3>Thank you for joining us at our Bangor town hall!</h3>} className="event-success-card" />
+            <div className="disable-screen">
+              <EventCard eventData={event2} loading={false} disabled />
+            </div>
           </Col>
         </Row>
       </MainLayout>
