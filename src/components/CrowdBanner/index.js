9a1c8534cc90ca148a48b82b686c0231755e469d
Alan Magelssen <alanmagelssen@gmail.com>
2019-09-29 14:04:16 -0700

    update crowd banner component

--- src/components/CrowdBanner/index.js
+++ src/components/CrowdBanner/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Card, Divider, Row, Col } from 'antd';
 
-// import Banner from '../../assets/images/crowdbanner.jpg';
 import './style.scss';
 
-const CrowdBanner = (props) => {
-  const {
-  } = props;
+const CrowdBanner = () => {
   return (
     <div className="crowd-banner-container"></div>
   );
 };
-
-CrowdBanner.propTypes = {
-
-};
-
 export default CrowdBanner;
