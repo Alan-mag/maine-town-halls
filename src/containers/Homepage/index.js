e493232f26d1d718c1c70ce9327464faa0b3b819
Alan Magelssen <alanmagelssen@gmail.com>
2019-09-30 12:10:19 -0700

    pull events from db

--- src/containers/Homepage/index.js
+++ src/containers/Homepage/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   Col,
   Row,
-  Card,
 } from 'antd';
 
 import EventCard from '../../components/EventCard';
@@ -12,6 +11,29 @@ import { firebasedb } from '../../utils/firebase';
 import { eventIds } from '../../constants';
 
 class Homepage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      event1: {},
+      event2: {},
+      loading: true,
+    };
+  }
+  componentDidMount() {
+    const event1Promise = firebasedb.ref(`townHalls/${eventIds[0]}`).once('value');
+    const event2Promise = firebasedb.ref(`townHalls/${eventIds[1]}`).once('value');
+    Promise.all([event1Promise, event2Promise])
+      .then(eventData => eventData.reduce((acc, cur, index) => {
+        acc[`event${index + 1}`] = cur.val();
+        return acc;
+      }, {}))
+      .then((data) => {
+        this.setState({
+          ...data,
+          loading: false,
+        });
+      });
+  }
 
   render() {
     const { event1, event2, loading } = this.state;
@@ -22,10 +44,7 @@ class Homepage extends React.Component {
             <EventCard eventData={event1} loading={loading} />
           </Col>
           <Col xs={24} sm={24} md={24} lg={12} xl={12}>
-            <Card title={<h3>Thank you for joining us at our Bangor town hall!</h3>} className="event-success-card" />
-            <div className="disable-screen">
-              <EventCard eventData={event2} loading={false} disabled />
-            </div>
+            <EventCard eventData={event2} loading={loading} />
           </Col>
         </Row>
       </MainLayout>
